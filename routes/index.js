b4c030a091a941f93f61abddf56bea6131fdbfaa
Favour Okedele <favouroked@gmail.com>
2018-09-23 11:20:07 +0100

    some changes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', url: 'https://documenter.getpostman.com/view/3647474/RWaPsm5F' });
 });
 
 module.exports = router;
